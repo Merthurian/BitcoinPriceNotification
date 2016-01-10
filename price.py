fe0a44c87edb0311e99676ce0d39d7a247b35ab2
Dan Purcell <dpurcell@veryhost.lan>
2016-01-10 19:48:30 +0000

    Changed to blockchain.info. Now reports: last sale, buy bids and sell requests

--- price.py
+++ price.py
@@ -3,6 +3,17 @@
 import requests
 from gi.repository import Notify
 
-r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+r = requests.get('https://blockchain.info/ticker')
 Notify.init("Price")
-Notify.Notification.new(u"Like, right now .. Bitcoins are like £" + r.json()['bpi']['GBP']['rate'] + " each.").show()
+
+last = float(r.json()['GBP']['last'])
+buy = float(r.json()['GBP']['buy'])
+sell = float(r.json()['GBP']['sell'])
+
+Notify.Notification.new(u"last: £" +
+    str(last) +
+    u" buy: £" +
+    str(buy) +
+    u" sell: £" +
+    str(sell)
+    ).show()
